12653ad091ef44b4b26fd61ad523b5eec6dcd54c
perlancar (@netbook-zenbook) <perlancar@gmail.com>
2016-04-01 01:37:22 +0700

    Initial commit

---
+++ lib/Bencher/Scenario/MojoDOM/Parsing.pm
@@ -0,0 +1,29 @@
+package Bencher::Scenario::MojoDOM::Parsing;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+use Bencher::ScenarioUtil::MojoDOM;
+
+our $scenario = {
+    summary => 'Benchmark parsing of HTML',
+    description => <<'_',
+
+Sample documents from `PERLANCAR::HTML::Tree::Examples` are used.
+
+_
+    participants => [
+        {
+            fcall_template => 'Mojo::DOM->new($Bencher::ScenarioUtil::MojoDOM::htmls{<size>})',
+        },
+    ],
+    datasets => \@Bencher::ScenarioUtil::MojoDOM::datasets,
+    include_result_size => 1,
+};
+
+1;
+# ABSTRACT:
---
+++ lib/Bencher/Scenarios/MojoDOM.pm
@@ -0,0 +1,9 @@
+package Bencher::Scenarios::MojoDOM;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Scenarios to benchmark Mojo::DOM
+
+=head1 SEE ALSO
