50b0b07497db827abe1d325302f33fb1b95cbf6b
perlancar (@netbook-zenbook) <perlancar@gmail.com>
2016-04-01 13:15:44 +0700

    Fixes

--- lib/Bencher/ScenarioUtil/MojoDOM.pm
+++ lib/Bencher/ScenarioUtil/MojoDOM.pm
@@ -4,8 +4,8 @@ package Bencher::ScenarioUtil::MojoDOM;
 # VERSION
 
 our @datasets = (
-    {name => 'small1' , summary => '16 elements, 4 levels, '.length($htmls{small1}).' bytes' , args=>{size=>'small1' }},
-    {name => 'medium1', summary => '20k elements, 7 levels, '.length($htmls{medium1}).' bytes', args=>{size=>'medium1'}},
+    {name => 'small1' , summary => '16 elements, 4 levels' , args=>{size=>'small1' }},
+    {name => 'medium1', summary => '20k elements, 7 levels', args=>{size=>'medium1'}},
 );
 
 # we want to record the version of these modules too in the benchmark result
